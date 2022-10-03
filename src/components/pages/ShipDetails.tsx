834bd6c1d1017f059999846629e6871c23a791ad
Masa H <masamasa0669@gmail.com>
2022-10-04 00:28:14 +0900

    Ship詳細を表示させる

--- src/components/pages/ShipDetails.tsx
+++ src/components/pages/ShipDetails.tsx
@@ -1,7 +1,39 @@
-export const ShipDetails = () => {
+import React, { memo, useState, useEffect, useContext } from "react"
+
+import {
+  Box,
+  Typography,
+  Button
+} from "@mui/material"
+
+import { useLocation, Link } from "react-router-dom"
+
+
+type State = {
+  id: number | undefined,
+  selectShip: string | undefined
+}
+
+export const ShipDetails = memo(() => {
+  const location = useLocation()
+  const {id, selectShip } = location.state as State
+
+
+  console.log("ship detailのレンダリング")
+
   return(
     <>
-      Ship Detailページ
+      <Box>
+        <Typography variant="h5" component="h5" sx={{mb:1}}>整備情報</Typography>
+        <Typography variant="h6" component="div" sx={{px:1}}>
+          <div>
+            選択中の機番 : {selectShip}
+          </div>
+            <Link to={`create`} state={{id: id, selectShip: selectShip}}>
+              <Button variant="contained">新規作成</Button>
+             </Link>
+        </Typography>
+      </Box>
     </>
   )
-}
+})
