a4ad47e369676166147cfc0ad5c545a7fbb1ecd4
Masa H <masamasa0669@gmail.com>
2022-10-03 10:36:22 +0900

    initical commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+import {Amplify} from 'aws-amplify';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
+
+export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>test</p>
     </div>
   );
 }
 
-export default App;
+
