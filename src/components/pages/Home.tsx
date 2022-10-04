728d81108e63bfce279cb87e5e64fe61a59d1c1a
Masa H <masamasa0669@gmail.com>
2022-10-04 12:37:45 +0900

    削除

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,30 +1,10 @@
 import { useState, useEffect } from 'react'
 
-import {Amplify, API, graphqlOperation } from "aws-amplify"
-import { listMaintenances } from 'graphql/queries'
-import awsExports from "aws-exports"
-
-Amplify.configure(awsExports)
-
-
 
 export const Home = () => {
   const [maintenances, setMaintenances] = useState([])
 
-  const fetchMaintenances = async() => {
-    try{
-      const maintenanceData: any = await API.graphql(graphqlOperation(listMaintenances))
-      const maintenances = maintenanceData.data.listMaintenances.items
-      setMaintenances(maintenances)
-    }catch(err){
-      console.log("error")
-    }
-  }
 
-  useEffect(()=> {
-    fetchMaintenances();
-  },[])
-  console.log(maintenances)
   return(
     <>
       Homeページ
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css';
 import  { App } from './App';
-import { Amplify } from "aws-amplify"
-import awsExports from "./aws-exports"
-Amplify.configure(awsExports)
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
