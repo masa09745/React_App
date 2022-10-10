26ff0f04c02667646041e2d8e2dea7e2f033b7d6
Masa H <masamasa0669@gmail.com>
2022-10-11 01:48:09 +0900

    サインイン後の画面遷移導入、Home画面でのサインアウトの挙動修正

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,17 +1,28 @@
 import { useState, useEffect } from 'react'
 
 import { useAuthenticator } from '@aws-amplify/ui-react'
+import { useNavigate } from 'react-router-dom'
+import { Button } from '@mui/material'
 
 
 export const Home = () => {
-  const { user , signOut} = useAuthenticator((context)=> [context.user]) 
+  const { user , signOut} = useAuthenticator((context)=> [
+    context.user,
+    context.signOut,
+  ]) 
 
+  const navigate = useNavigate()
+
+  const logOut =  async(e: React.MouseEvent<HTMLButtonElement>) => {
+    signOut();
+    navigate('/signin');
+  }
 
   return(
     <>
-      Homeページ {user.username}
+      Homeページ {user?.username}
 
-      <button onClick={signOut}>SignOut</button>
+      <Button  onClick={logOut}>サインアウト</Button>
     </>
   )
 }
--- src/components/pages/SignIn.tsx
+++ src/components/pages/SignIn.tsx
@@ -1,6 +1,17 @@
-import { Authenticator, } from "@aws-amplify/ui-react"
+import { Authenticator, useAuthenticator, } from "@aws-amplify/ui-react"
+import { useEffect } from "react"
+import { useNavigate, useLocation } from "react-router-dom"
 
 export const SignIn = () => {
+  const { route } = useAuthenticator((context) => [context.route])
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  useEffect (() => {
+    if (route === 'authenticated') {
+      navigate("/ships", {replace: true})
+    }
+  }, [route, navigate])
 
   return (
     <Authenticator hideSignUp={true}></Authenticator>
